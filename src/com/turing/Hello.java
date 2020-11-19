c48e06001e5ddb8916f847c118a8176ec897d650
chenzhe <chenzhe@163.com>
2020-11-19 21:23:06 +0800

    java18

--- src/com/turing/Hello.java
+++ src/com/turing/Hello.java
@@ -4,5 +4,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("java17");
         System.out.println("java172");
+        System.out.println("java18");
     }
 }
