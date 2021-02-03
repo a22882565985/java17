41fecda8cd0b4c2ac69262f1d227feb351f964c2
chenzhe <chenzhe@163.com>
2021-02-03 21:38:40 +0800

    java20

--- src/com/turing/Hello.java
+++ src/com/turing/Hello.java
@@ -2,10 +2,37 @@ package com.turing;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("java17");
-        System.out.println("java172");
-        System.out.println("java18");
-        System.out.println("java19");
-        System.out.println("java20");
+//        System.out.println("java17");
+//        System.out.println("java172");
+//        System.out.println("java18");
+//        System.out.println("java19");
+//        System.out.println("java20");
+
+        /*for(int i=0;i>-1;i++){
+
+            System.out.println(i+"Hello, world");
+
+        }*/
+
+
+//        Integer num1 = 128;
+//        Integer num2 = 128;
+        /*String str1 = new String("10");
+        String str2 = new String("10");*/
+//        System.out.println((num1==num2)+",");
+
+//        System.out.println('1'+1);//50
+//        System.out.println('a'+1);//98
+//        System.out.println('A'+1);//66
+
+//        int num = (3<<5)-3;
+//        System.out.println(num);
+
+        int a = 10;
+        int b = 10;
+        System.out.println(a==b);
+
     }
+
+
 }
